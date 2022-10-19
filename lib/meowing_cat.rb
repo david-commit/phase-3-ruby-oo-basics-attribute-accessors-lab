34d33d9cf8ef108d4995c15773c2635f2787484b
david-commit <david.ondiege@students.moringaschool.com>
2022-10-19 16:37:00 +0300

    Completed code-along

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
 ## code your solution here. 
+require "pry"
+class Cat
+ attr_accessor :name
+ attr_writer :meow
+
+ def name
+  @name
+ end
+
+ def meow
+  puts "meow!"
+ end
+ 
+end
+# binding.pry
